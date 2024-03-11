e9a1aba51e32a506670fd6d204101a9d4967a542
margarida-dias <margaridadiasfernandes@gmail.comm>
2024-03-11 19:56:50 +0000

    feat: create artifacts

--- predict.py
+++ predict.py
@@ -1,21 +1,20 @@
 from ultralytics import YOLO
 import os
 
-if __name__ == "__main__":
 
-    path_to_model = "./valohai/outputs/best.onnx"
-    path_to_images = "./valohai/inputs/images/"
+path_to_model = "./valohai/outputs/best.onnx"
+path_to_images = "./valohai/inputs/images/"
 
-    model = YOLO(path_to_model, task="detect")
+model = YOLO(path_to_model, task="detect")
 
-    for image in os.listdir(path_to_images):
-        image_path = os.path.join(path_to_images, image)
+for image in os.listdir(path_to_images):
+    image_path = os.path.join(path_to_images, image)
 
-        if os.path.isfile(image_path):
-            # Run prediction on image
-            # Save result as an image in /valohai/outputs/
-            results = model.predict(image_path, save=True, project="./valohai/outputs", name="predictions")
+    if os.path.isfile(image_path):
+        # Run prediction on image
+        # Save result as an image in /valohai/outputs/
+        results = model.predict(image_path, save=True, project="./valohai/outputs", name="predictions")
 
-            # Print the Boxes object containing the detection bounding boxes
-            for r in results:
-                print(r.boxes)
+        # Print the Boxes object containing the detection bounding boxes
+        for r in results:
+            print(r.boxes)
